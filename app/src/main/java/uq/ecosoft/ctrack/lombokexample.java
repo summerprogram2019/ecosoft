45d857b0bd223ce0770d2116d4d9e7e108bdd79b
Harry Guthrie <GTGuthers@gmail.com>
2019-07-03 18:53:50 +1000

    style fixes and changes

--- app/src/main/java/uq/ecosoft/ctrack/lombokexample.java
+++ app/src/main/java/uq/ecosoft/ctrack/lombokexample.java
@@ -5,13 +5,19 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 
-@RequiredArgsConstructor @AllArgsConstructor
+@RequiredArgsConstructor @AllArgsConstructor //@Log4j
 public class lombokexample {
+
     @Getter @Setter @NonNull
     private String foo;
 
     @Getter @Setter
     private String bar;
+
+    private void method1() {
+//        log.info("foobar");
+    }
 
 }
