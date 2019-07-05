d5e3fce14730a16913716ba179090e31ff0e559b
jhhuang7 <juhua.huang@uqconnect.edu.au>
2019-07-05 22:55:49 +1000

    Added basic page linking
    Added methods in MainActivity.java

--- app/src/main/java/uq/ecosoft/ctrack/MainActivity.java
+++ app/src/main/java/uq/ecosoft/ctrack/MainActivity.java
@@ -2,6 +2,7 @@ package uq.ecosoft.ctrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,6 +13,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void linkToHome(View view) {
+        setContentView(R.layout.home);
+    }
+
+    public void linkToSignUp(View view) {
+        setContentView(R.layout.sign_up);
+    }
+
+    public void linkToVerify(View view) {
+        setContentView(R.layout.verify);
+    }
+
+    public void linkToActivities(View view) {
+        setContentView(R.layout.activities_home);
+    }
+
+    public void linkToSteps(View view) {
+        setContentView(R.layout.step_tracking);
+    }
+
+    public void linkToGoals(View view) {
+        setContentView(R.layout.goals);
+    }
+
+    public void linkToChallenges(View view) {
+        setContentView(R.layout.daily_challenges);
+    }
+
+    public void linkToFuture(View view) {
+        setContentView(R.layout.future_activities);
+    }
+
+    public void linkToSocial(View view) {
+        setContentView(R.layout.social_leaderboard);
+    }
+
+    public void linkToFriends(View view) {
+        setContentView(R.layout.social_friends);
+    }
+
+    public void linkToProfile(View view) {
+        setContentView(R.layout.profile);
     }
 
     public void basicBtn(View view) {
