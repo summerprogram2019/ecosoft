5d5e8697eccf62fd57ffe789d0e6c9c1839513de
Robert Fraser <robert.fraser@uqconnect.edu.au>
2019-07-08 23:23:33 +0800

    Mild work on ArticleDatabase
    Adds functionality for liking and unliking articles & also scaffolds a few additional functions, especially for working directly with the Article class

--- app/src/main/java/uq/ecosoft/ctrack/controller/ArticleDatabase.java
+++ app/src/main/java/uq/ecosoft/ctrack/controller/ArticleDatabase.java
@@ -1,5 +1,11 @@
 package uq.ecosoft.ctrack.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import uq.ecosoft.ctrack.model.Article;
+
 public class ArticleDatabase {
     private DatabaseConnector connector;
 
@@ -23,6 +29,10 @@ public class ArticleDatabase {
         return false;
     }
 
+    public Boolean addNewArticle(Article article) {
+        return false;
+    }
+
     /**
      * Removes a given article from the database
      * @param articleID ID of the article to remove
@@ -32,12 +42,14 @@ public class ArticleDatabase {
         return false;
     }
 
+    public Boolean removeArticle(Article article) { return false; }
+
     /**
      * Retrieve the contents of a given article
      * @param articleID ID of the article
      */
     public void getArticle(int articleID) {
-
+        // return new Article();
     }
 
     /**
@@ -48,11 +60,42 @@ public class ArticleDatabase {
 
     }
 
+    /**
+     * Get a total number of likes of the article
+     * @param articleID
+     */
     public void getArticleLikes(int articleID) {
 
     }
 
-    public void likeArticle(int userID, int articleID) {
+    /**
+     * Store that a user likes an article
+     * @param userID
+     * @param articleID
+     */
+    public Boolean likeArticle(int userID, int articleID) throws SQLException {
+        Connection con = connector.getConnection();
 
+        PreparedStatement q = con.prepareStatement("INSERT INTO liked_articles (uid, article) VALUES(?, ?)");
+        q.setInt(1, userID);
+        q.setInt(2, articleID);
+        int result = q.executeUpdate();
+        return (result == 1);
+    }
+
+    /**
+     * Remove a stored like from the article
+     * @param userID
+     * @param articleID
+     * @return whether or not the like was successfully removed
+     */
+    public Boolean unlikeArticle(int userID, int articleID) throws SQLException {
+        Connection con = connector.getConnection();
+
+        PreparedStatement q = con.prepareStatement("DELETE FROM liked_articles WHERE uid=? AND article=?");
+        q.setInt(1, userID);
+        q.setInt(2, articleID);
+        int result = q.executeUpdate();
+        return (result == 1);
     }
 }
