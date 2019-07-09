3b13fb80dbefd732fda22f3f024acd0881c4f248
Robert Fraser <robert.fraser@uqconnect.edu.au>
2019-07-09 13:53:54 +0800

    DatabaseConnector improvements
    To help improve optimisation, the connecting methods have been improved substantially. Additionally, convience functions to easily close the connection when done are provided. For best optimisation, after queries the connection should be closed.

--- app/src/main/java/uq/ecosoft/ctrack/controller/DatabaseConnector.java
+++ app/src/main/java/uq/ecosoft/ctrack/controller/DatabaseConnector.java
@@ -2,17 +2,17 @@ package uq.ecosoft.ctrack.controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
     /**
      * Database connector that manages the connection to the SQL database
      */
-    private static String DB_URL = "jdbc:mysql://localhost:3306/ecosoft";
-    private static String DB_USERNAME = "root";
-    private static String DB_PASSWORD = "supercoolpassword";
-
-    private Connection connection = null;
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/ecosoft";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "supercoolpassword";
 
     /**
      * Get a connection to the database
@@ -20,13 +20,55 @@ public class DatabaseConnector {
      * @return Connection to the database
      * @throws SQLException
      */
-    public Connection getConnection() throws SQLException {
-        // If already connected, return that
-        if(connection != null) {
-            return connection;
-        }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
 
-        connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-        return connection;
+    /**
+     * Helper function for closing a database connection when done with it
+     * This takes in the ResultSet, PreparedStatement, and Connection
+     * This quietly ignores errors that are thrown
+     * @param rs ResultSet to close
+     * @param ps PreparedStatement to close
+     * @param con Connection to close
+     */
+    public static void closeQuietly(ResultSet rs, PreparedStatement ps, Connection con) {
+        try {
+            rs.close();
+            ps.close();
+            con.close();
+        } catch (SQLException e) {
+            // ignore
+        }
+    }
+
+    /**
+     * Helper function for closing a database connection when done with it
+     * This takes in the PreparedStatement and Connection
+     * This quietly ignores errors that are thrown
+     * @param ps PreparedStatement to close
+     * @param con Connection to close
+     */
+    public static void closeQuietly(PreparedStatement ps, Connection con) {
+        try {
+            ps.close();
+            con.close();
+        } catch (SQLException e) {
+            // ignore
+        }
+    }
+
+    /**
+     * Helper function for closing a database connection when done with it
+     * This takes in only the Connection itself
+     * This quietly ignores errors that are thrown
+     * @param con Connection to close
+     */
+    public static void closeQuietly(Connection con) {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            // ignore
+        }
     }
 }
