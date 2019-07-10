56d05d23aff0ceb52f7e3256af6587b3cf567597
Harry Guthrie <GTGuthers@gmail.com>
2019-07-10 13:19:06 +1000

    Formatting changes

--- app/src/main/java/uq/ecosoft/ctrack/model/activities/Activity.java
+++ app/src/main/java/uq/ecosoft/ctrack/model/activities/Activity.java
@@ -5,25 +5,26 @@ import lombok.Getter;
 import lombok.extern.java.Log;
 
 @AllArgsConstructor @Log
-public enum ActivityType {
+public enum Activity {
     /**
      * These are the preset enums
      */
-    WALKING(0, "Walking",
-            "If you take steps you are walking","steps",10),
-    DRIVING(1, "Driving", "If it goes vroom you drive",
-            "minutes", -100);
+    WALKING(1,  "act_step","Walking",
+            "If you take steps you are walking","steps",1),
+    DRIVING(2, "act_drive", "Driving", "If it goes vroom you drive",
+            "minutes", -50),
+    BUS(3, "act_nostraw", "Straw Denial", "You helped prevent plastic waste",
+                    "straws", -10),
+    NO_STRAW(4, "act_bus", "Taking the bus",
+            "If it goes vroom and you're with lots of others, you're in a bus.","minutes",
+            20);
 
-    @Getter
-    Integer id;
-    @Getter
-    String name;
-    @Getter
-    String description;
-    @Getter
-    String unitName;
-    @Getter
-    Integer greenValue;
+    @Getter Integer id;
+    @Getter String key;
+    @Getter String name;
+    @Getter String description;
+    @Getter String unitName;
+    @Getter Integer greenValue;
 
     public Boolean isGreen() {
         return (greenValue > 0);
--- app/src/main/java/uq/ecosoft/ctrack/model/garden/PlantInstance.java
+++ app/src/main/java/uq/ecosoft/ctrack/model/garden/PlantInstance.java
@@ -1,4 +1,10 @@
 package uq.ecosoft.ctrack.model.garden;
 
+import java.util.Date;
+
+import lombok.NonNull;
+
 public class PlantInstance {
+    @NonNull Plant plant;
+    @NonNull Date time;
 }
