1a45ce540b9338649f41ca8dae3cd0f78571d268
Robert Fraser <robert.fraser@uqconnect.edu.au>
2019-07-10 11:22:03 +0800

    Closing connection handles nulls

--- app/src/main/java/uq/ecosoft/ctrack/controller/DatabaseConnector.java
+++ app/src/main/java/uq/ecosoft/ctrack/controller/DatabaseConnector.java
@@ -54,6 +54,8 @@ public class DatabaseConnector {
             con.close();
         } catch (SQLException e) {
             // ignore
+        } catch (NullPointerException e) {
+            // ignore
         }
     }
 
