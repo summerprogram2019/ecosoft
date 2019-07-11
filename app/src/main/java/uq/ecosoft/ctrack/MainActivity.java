1de48071b8bc6ae43c851b6b7e94a17dbec9f424
Sunny <sunnbobby@gmail.com>
2019-07-11 11:06:49 +1000

    Finished Setting bar, navbar, started activities view
    .. settingbar.xml
    .. navbar.xml
    .. activities_home2.xml
    + daily_challenges2.xml
    + future_activities2.xml

--- app/src/main/java/uq/ecosoft/ctrack/MainActivity.java
+++ app/src/main/java/uq/ecosoft/ctrack/MainActivity.java
@@ -1,18 +1,17 @@
 package uq.ecosoft.ctrack;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
     public void linkToLogin(View view) {
